c297da4e39f6e8335997a0718c24d0154b4b7715
Mohamad Harits Nur Fauzan <mharitsnf@gmail.com>
2021-05-04 20:13:03 +0700

    Add introductions

--- src/HUD/introductions.gd
+++ src/HUD/introductions.gd
@@ -1,9 +1,10 @@
 extends Control
 
+
 func _ready():
 	Globals.current_level = self
 
 func _physics_process(_delta):
 	if Input.is_action_pressed("jump"):
-		Globals.root.change_level_to("res://src/HUD/Introductions.tscn", false)
+		Globals.root.change_level_to("res://src/levels/Level1.tscn", false)
 		set_physics_process(false)
